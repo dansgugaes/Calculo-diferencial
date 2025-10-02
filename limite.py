ce287fd30151d349638b23fcefcd14e16dcdab79
dansgugaes <daniela.ge@uinenlinea.mx>
2025-10-02 14:33:41 +0000

    programa limites

---
+++ limite.py
@@ -0,0 +1,23 @@
+#Calculo de limites 
+#punto de analisis 
+xd=1.51
+xi=1.51
+xinf=1.51
+#evaluacion derecha
+for i in range(1,10):
+
+    xd=xd+0.01
+    xi=xi-0.01
+    xinf=xinf+1000
+    #funcion
+    yd=(5*xd**3+2)/(2*xd**3-7)
+    yi=(5*xi**3+2)/(2*xi**3-7)
+    yinf=(5*xinf**3+2)/(2*xinf**3-7)
+    print(yi,yd,yinf)
+
+
+
+
+
+
+
